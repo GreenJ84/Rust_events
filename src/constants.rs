db064db674618ed42434bffd34a7e14f38ca3644
Jesse Greenough <jesse.greenough84@gmail.com>
2025-09-20 13:45:37 -0700

    Add separate module for crate type constants (base data)

---
+++ src/constants.rs
@@ -0,0 +1,51 @@
+#[cfg(feature = "threaded")]
+use std::sync::Arc;
+
+#[cfg(not(feature = "threaded"))]
+extern crate alloc;
+#[cfg(not(feature = "threaded"))]
+use alloc::sync::Arc;
+
+/// Type alias for an event payload pointer.
+///
+/// Uses `Arc<T>` for both threaded and embedded builds.
+///
+/// # Example
+/// ```
+/// use alloc::sync::Arc; //(embedded/no_std)
+
+/// use std::sync::Arc;  //(threaded)
+/// use events::{Callback, EventPayload};
+/// let payload: EventPayload<String> = Arc::new(String::from("Emitting value"));
+/// ```
+pub type EventPayload<T> = Arc<T>;
+
+
+/// Type alias for a callback pointer.
+///
+/// - Allows any closure (no thread-safety required).
+///
+/// # Example (embedded/no_std)
+/// ```
+/// use alloc::sync::Arc;
+/// use events::{Callback, EventPayload};
+/// let callback: Callback<String> = Arc::new(move |payload: &EventPayload<String>| {
+///     println!("Received event: {}", payload);
+/// });
+/// ```
+#[cfg(not(feature = "threaded"))]
+pub type Callback<T> = Arc<dyn Fn(&EventPayload<T>)>;
+
+/// Type alias for a callback pointer.
+///
+/// - Requires `Send + Sync` for thread safety.
+///
+/// # Example (threaded)
+/// ```
+/// use std::sync::Arc;
+/// use events::{Callback, EventPayload};
+/// let callback: Callback<String> = Arc::new(move |payload: &EventPayload<String>| {
+///     println!("Received event: {}", payload);
+/// });
+#[cfg(feature = "threaded")]
+pub type Callback<T> = Arc<dyn Fn(&EventPayload<T>) + Send + Sync>;
