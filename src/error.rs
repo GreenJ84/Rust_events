cf30e552f573fdfa93a044d4acce890608fda540
Jesse Greenough <jesse.greenough84@gmail.com>
2025-09-20 13:43:29 -0700

    Add separate module for error variant enum - Implement Display trait for Error enum - feature lock Customer std Error variant for std environments only

---
+++ src/error.rs
@@ -0,0 +1,46 @@
+/// Errors that can occur in the event system.
+///
+/// - `OverloadedEvent`: Too many listeners for an event.
+/// - `ListenerNotFound`: Tried to remove or emit to a listener that does not exist.
+/// - `EventNotFound`: Tried to remove or emit to an event that does not exist.
+/// - `Other`: Any other error (boxed).
+#[derive(Debug)]
+pub enum EventError {
+    /// Adding Listener:
+    /// - Trying to add more than `max_listeners` to an Event.
+    OverloadedEvent,
+    /// Removing Listener/Emitting Event:
+    /// - Trying to access a specific `Listener` that cannot be found.
+    ListenerNotFound,
+    /// Removing Listener/Emitting Event:
+    /// Trying to access a specific `Event` that cannot be found.
+    EventNotFound,
+    /// Any other possible Errors during Event Handling
+    #[cfg(feature = "threaded")]
+    Other(Box<dyn std::error::Error + Send + Sync>),
+}
+impl PartialEq for EventError {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (EventError::ListenerNotFound, EventError::ListenerNotFound) |
+            (EventError::EventNotFound, EventError::EventNotFound) |
+            (EventError::OverloadedEvent, EventError::OverloadedEvent) => true,
+            (EventError::Other(a), EventError::Other(b)) => a.to_string() == b.to_string(),
+            _ => false
+        }
+    }
+}
+impl Eq for EventError {}
+
+impl core::fmt::Display for EventError {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self {
+            EventError::OverloadedEvent => write!(f, "Too many listeners for event"),
+            EventError::ListenerNotFound => write!(f, "Listener not found"),
+            EventError::EventNotFound => write!(f, "Event not found"),
+            EventError::Other(e) => write!(f, "Other error: {}", e),
+        }
+    }
+}
+
+impl std::error::Error for EventError {}
